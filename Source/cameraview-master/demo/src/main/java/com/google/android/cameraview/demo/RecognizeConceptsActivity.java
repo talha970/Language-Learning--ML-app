94f46d7edbf7de88abb1855187cf473530117e6c
talha970 <talha970@gmail.com>
2017-03-02 15:18:13 -0600

    Cameraview added

---
+++ Source/cameraview-master/demo/src/main/java/com/google/android/cameraview/demo/RecognizeConceptsActivity.java
@@ -0,0 +1,13 @@
+package com.google.android.cameraview.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecognizeConceptsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recognize_concepts);
+    }
+}
